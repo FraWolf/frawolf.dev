763283a6757bb019111060e405252d8a9d751fcf
FraWolf <frawolf@outlook.it>
2025-10-05 22:22:51 +0200

    WIP: Migration from old Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+
+    imageService: "cloudflare",
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
