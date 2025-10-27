2845e01d016f28ee9f9c6bd7795b0a08829a2321
FraWolf <frawolf@outlook.it>
2025-10-27 21:28:10 +0100

    Removed unused data

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,6 @@
 import { defineCollection } from "astro:content";
-import { file, glob } from "astro/loaders";
+import { file } from "astro/loaders";
 import { projectSchema } from "./schemas/content";
-
-// const projects = defineCollection({
-//   loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
-//   schema: projectSchema,
-// });
 
 const projectsJson = defineCollection({
   loader: file("src/data/projects/data.json", {
@@ -21,10 +16,5 @@ const projectsJson = defineCollection({
   }),
   schema: projectSchema,
 });
-
-// const blog = defineCollection({
-//   loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
-//   schema: projectSchema,
-// });
 
 export const collections = { projects: projectsJson };
